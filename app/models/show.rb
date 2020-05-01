b5c424951603933e3f954d94d4887d4bb0b5e7e0
rwparrish <rino3717@gmail.com>
2020-04-30 20:09:13 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    full_names = []
+    actors_names = self.actors.pluck(:first_name, :last_name).flatten.join(" ")
+    full_names << actors_names 
+  end
 end
